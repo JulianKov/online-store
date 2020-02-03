5869b6c0a169c61011174272b7dc977426e85eee
Julian <yulian_kov@gmail.com>
2020-02-03 22:22:16 +0300

    Updates

---
+++ precache-manifest.6d9175876d7ffc761b9f9d2f606f0eff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874b9da71127c6faee7c33680620339e",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "c11444dc964d7550be67",
+    "url": "/online-store/static/css/main.c33b5cad.chunk.css"
+  },
+  {
+    "revision": "b0596f3e74e771971483",
+    "url": "/online-store/static/js/2.279222f1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/online-store/static/js/2.279222f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c11444dc964d7550be67",
+    "url": "/online-store/static/js/main.749c390f.chunk.js"
+  },
+  {
+    "revision": "355e662f45e9ccbc2d83",
+    "url": "/online-store/static/js/runtime-main.88cae5ac.js"
+  }
+]);
